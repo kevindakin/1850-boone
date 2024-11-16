f2b8024b73eee062f4025a1c8df66972709d4b3f
kevindakin <142453268+kevindakin@users.noreply.github.com>
2024-11-16 05:45:00 -0800

    Updated Hero Animations
    Updated GSAP timelines for all hero components.

---
+++ hero-blog.js
@@ -0,0 +1,76 @@
+function heroBlog() {
+    const loaderBlock = document.querySelector('[load-el="loader-block"]');
+    const backgroundImage = document.querySelector('[load-el="image"]');
+    const nav = document.querySelector(".nav_component");
+    const heading = document.querySelector('[load-el="split"]');
+    const fadeEls = document.querySelectorAll('[load-el="fade-in"]');
+  
+    let splitText = [];
+    if (heading) {
+      const headlineSplit = new SplitType(heading, {
+        types: "lines, words",
+        tagName: "span",
+      });
+      splitText = heading.querySelectorAll(".word");
+    }
+  
+    const loadAnim = gsap.timeline({
+      defaults: {
+        duration: 1.6,
+        ease: easeInOut,
+      },
+    });
+  
+    loadAnim
+      .to(
+        loaderBlock,
+        {
+          opacity: "0",
+          duration: durationSlow,
+        },
+        ">"
+      )
+  
+      .from(
+        backgroundImage,
+        {
+          scale: 1.4,
+          ease: easeInOut,
+          duration: 3,
+        },
+        "<-0.5"
+      )
+  
+      .from(
+        nav,
+        {
+          y: "-3rem",
+          opacity: 0,
+        },
+        "<0.5"
+      )
+  
+      .from(
+        splitText,
+        {
+          y: "110%",
+          stagger: 0.07,
+        },
+        "<0.5"
+      )
+  
+      .to(
+        fadeEls,
+        {
+          opacity: 1,
+          stagger: 0.2,
+        },
+        "<0.5"
+      );
+  
+    loadAnim.set(loaderBlock, { display: "none" });
+  }
+  
+  document.addEventListener("DOMContentLoaded", (event) => {
+    heroBlog();
+  });
--- hero-v4.js
+++ hero-v4.js
@@ -1,77 +1,76 @@
 function heroV4() {
-    const loaderBlock = document.querySelector('[load-el="loader-block"]');
-    const backgroundImage = document.querySelector('[load-el="image"]');
-    const nav = document.querySelector(".nav_component");
-    const heading = document.querySelector('[load-el="split"]');
-    const fadeEls = document.querySelectorAll('[load-el="fade-in"]');
-  
-    let splitText = [];
-    if (heading) {
-      const headlineSplit = new SplitType(heading, {
-        types: "lines, chars",
-        tagName: "span",
-      });
-      splitText = heading.querySelectorAll(".char");
-    }
-  
-    const loadAnim = gsap.timeline({
-      defaults: {
-        duration: 1.6,
-        ease: easeInOut,
-      },
+  const loaderBlock = document.querySelector('[load-el="loader-block"]');
+  const backgroundImage = document.querySelector('[load-el="image"]');
+  const nav = document.querySelector(".nav_component");
+  const heading = document.querySelector('[load-el="split"] > h2');
+  const fadeEls = document.querySelectorAll('[load-el="fade-in"]');
+
+  let splitText = [];
+  if (heading) {
+    const headlineSplit = new SplitType(heading, {
+      types: "lines, chars",
+      tagName: "span",
     });
-  
-    loadAnim
-      .to(
-        loaderBlock,
-        {
-          opacity: "0",
-          duration: durationSlow,
-        },
-        ">"
-      )
-  
-      .from(
-        backgroundImage,
-        {
-          scale: 1.4,
-          ease: easeInOut,
-          duration: 3,
-        },
-        "<-0.5"
-      )
-  
-      .from(
-        nav,
-        {
-          y: "-3rem",
-          opacity: 0,
-        },
-        "<0.5"
-      )
-  
-      .from(
-        splitText,
-        {
-          y: "110%",
-          stagger: 0.05,
-        },
-        "<0.5"
-      )
-  
-      .to(
-        fadeEls,
-        {
-          opacity: 1,
-          stagger: 0.3,
-        },
-        "<1"
-      );
-  
-    loadAnim.set(loaderBlock, { display: "none" });
+    splitText = heading.querySelectorAll(".char");
   }
-  
-  document.addEventListener("DOMContentLoaded", (event) => {
-    heroV4();
+
+  const loadAnim = gsap.timeline({
+    defaults: {
+      duration: 1.6,
+      ease: easeInOut,
+    },
   });
-  
+
+  loadAnim
+    .to(
+      loaderBlock,
+      {
+        opacity: "0",
+        duration: durationSlow,
+      },
+      ">"
+    )
+
+    .from(
+      backgroundImage,
+      {
+        scale: 1.4,
+        ease: easeInOut,
+        duration: 3,
+      },
+      "<-0.5"
+    )
+
+    .from(
+      nav,
+      {
+        y: "-3rem",
+        opacity: 0,
+      },
+      "<0.5"
+    )
+
+    .from(
+      splitText,
+      {
+        y: "110%",
+        stagger: 0.05,
+      },
+      "<0.5"
+    )
+
+    .to(
+      fadeEls,
+      {
+        opacity: 1,
+        stagger: 0.3,
+      },
+      "<0.5"
+    );
+
+  loadAnim.set(loaderBlock, { display: "none" });
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  heroV4();
+});
